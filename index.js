aeaa58b7eab6e7f25b86458e8f4431420d0bd4ba
masu <graphicgorillaz@gmail.com>
2024-03-11 11:05:01 +0200

    Set /add API endpoint to receive two values

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
   const currentUser = await getCurrentUser();
+  
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
